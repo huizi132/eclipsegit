0758c11508edfd55a2bb9a0599fa2f81f7fbec39
yangjie <yangjie132@126.com>
2020-06-18 17:45:57 +0800

    order_branch add TestOrder.java

---
+++ gittest/src/main/java/com/youceedu/test/order/TestOrder.java
@@ -0,0 +1,11 @@
+package com.youceedu.test.order;
+
+public class TestOrder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TestOrder");
+
+	}
+
+}
